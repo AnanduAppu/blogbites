43fc46b5771581dfbc25fedfd7237d027f57bd31
Anandu Sankar <ananduap74@gmail.com>
2024-06-17 16:24:47 +0530

    17/06/2024

--- edithBlogs.js
+++ edithBlogs.js
@@ -1,3 +1,6 @@
+function edithBlogs() {
+  return (
+
 <div class="container relative mx-auto mt-5 flex h-full max-w-6xl flex-col justify-between px-10 xl:px-0">
   <h2 class="mb-1 text-3xl font-extrabold leading-tight text-gray-900">Edit Blog</h2>
 
@@ -10,26 +13,18 @@
             <textarea id="message" rows="4" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500" placeholder="Write your thoughts here..."></textarea>
           </div>
         </div>
-      </div>
-      <div class="w-full sm:w-1/2">
-        <div class="relative ml-0 h-full md:mr-10">
-          <span class="absolute left-0 top-0 ml-1 mt-1 h-full w-full rounded-lg bg-purple-500"></span>
-          <div class="relative h-full rounded-lg border-2 border-purple-500 bg-white p-5">
-            <div class="grid grid-cols-5 gap-4 md:grid-cols-5">
-              <div class="relative">
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="Sample Image" />
-                <div class="absolute inset-0 flex items-center justify-center">
-                  <span class="font-bold text-white">Your Text Here</span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+     
     <div class=" flex w-[97%] max-sm:w-[100%] flex-col border border-green-500 sm:flex-row rounded-md">
       <textarea id="message" rows="15" class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500" placeholder="Write your thoughts here..."></textarea>
     </div>
   </div>
 </div>
 
+
+
+
+
+  )
+}
+
+export default edithBlogs
