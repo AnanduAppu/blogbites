62dead83b3ae03d7f5b76ee14ab16dd50209e35e
Anandu Sankar <ananduap74@gmail.com>
2024-07-23 14:41:05 +0530

    23-07-2024

--- src/Login&signup/OpenNavbar.jsx
+++ src/Login&signup/OpenNavbar.jsx
@@ -20,11 +20,11 @@ function OpenNavbar() {
   return (
     <nav className="bg-white border-gray-200 ">
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      
+      <span className="flex items-center space-x-3 rtl:space-x-reverse">
         <img src={imagelogo} className="w-32 " alt="Flowbite Logo" />
         <span class="self-center text-3x1 font-bold whitespace-nowrap dark:text-white">BLOG <span className='text-blue-700'>BITES</span> </span>
        
-  
+      </span>
       <button
         data-collapse-toggle="navbar-default"
         type="button"
@@ -38,7 +38,7 @@ function OpenNavbar() {
         </svg>
       </button>
       <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white   dark:border-gray-700">
           <li>
             <Link to="/open" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
           </li>
