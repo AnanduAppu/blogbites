e2c39c552ad27d006a5d6d331689d8c8edc401d1
Anandu Sankar <ananduap74@gmail.com>
2024-07-23 15:29:53 +0530

    23-07-2024

--- src/Login&signup/loginform.jsx
+++ src/Login&signup/loginform.jsx
@@ -102,6 +102,7 @@ function Loginform() {
         <hr className="my-6 border-gray-300 w-full" />
         <div className="w-full lg:w-[45%] sm:w-[60%] max-sm:w-[90%] mx-auto">
       <GoogleLogin
+      width='400px'
         onSuccess={(credentialResponse) => {
           var value = jwtDecode(credentialResponse.credential);
           console.log(value);
