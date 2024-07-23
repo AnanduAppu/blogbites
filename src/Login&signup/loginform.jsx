d45aa9021eab9d3fda756b9d27fd07bd9f27892f
Anandu Sankar <ananduap74@gmail.com>
2024-07-23 15:22:19 +0530

    23-07-2024

--- src/Login&signup/loginform.jsx
+++ src/Login&signup/loginform.jsx
@@ -100,7 +100,7 @@ function Loginform() {
         </form>
 
         <hr className="my-6 border-gray-300 w-full" />
-      <div className="w-[45%] sm:w-[37%]">
+      <div className="lg:w-[45%] sm:w-[30%]">
       <GoogleLogin
           width="500px"
           onSuccess={(credentialResponse) => {
